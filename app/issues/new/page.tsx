1e3351f74d904302a3f22d138a75d083ea1abdfb
Trieu <phamhai.trieu@hachi-x.com>
2024-05-25 21:52:55 +0700

    :v: [front] New issue: Change description to markdown

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,7 +1,8 @@
 "use client"
 
-import { Button, TextArea, TextField } from '@radix-ui/themes'
-import React from 'react'
+import { Button,  TextField } from '@radix-ui/themes';
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const NewIssuePage = () => {
 	return (
@@ -12,7 +13,7 @@ const NewIssuePage = () => {
 					className='max-w-xl'
 					/>
 			</TextField.Root>
-			<TextArea placeholder='Description' />
+			<SimpleMDE placeholder='Description' />
 			<Button>Submit new issue</Button>
 		</div>
 	)
