c42adf7e8a19d651b2dfb90860db53c6e210af22
Trieu <phamhai.trieu@hachi-x.com>
2024-05-25 18:23:28 +0700

    :sparkles: [back] Db: API for creating issues

---
+++ app/api/issues/route.ts
@@ -0,0 +1,23 @@
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const createIssueSchema = z.object({
+	title: z.string().min(1).max(255),
+	description: z.string().min(1)
+});
+
+export async function POST(request: NextRequest) {
+	const body = await request.json();
+	const validate = createIssueSchema.safeParse(body);
+	if (!validate.success)
+		return NextResponse.json(validate.error.errors, { status: 400 })
+
+	const newIssue = await prisma.issues.create({
+		data: {
+			title: body.title,
+			description: body.description
+		}
+	})
+	return NextResponse.json(newIssue, { status: 201 })
+}
