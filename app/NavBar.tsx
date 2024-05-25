87643b5b1c3816a3e5d0676ce22954f7e880f364
Trieu <phamhai.trieu@hachi-x.com>
2024-05-25 17:23:06 +0700

    :v: [front] NavBar: Finishing up the basics

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,25 +1,36 @@
+"use client";
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 import { IoBugSharp } from "react-icons/io5";
+import classnames from "classnames";
 
 
 const NavBar = () => {
+	const currentPath = usePathname();
+	console.log(currentPath)
+
 	const links = [
-		{ label: "Dashboard", href: "#"},
-		{ label: "Issues", href: "#"}
+		{ label: "Dashboard", href: "/"},
+		{ label: "Issues", href: "/issues"}
 	];
 	return (
 		<nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
 			<Link href="/"><IoBugSharp /></Link>
 			<ul className='flex space-x-6'>
-				{links.map(nav_item => {
+				{links.map(item => {
 					return (
-						<li key={nav_item.href} className='group'>
+						<li key={item.href} className='group'>
 							<Link
-								href={nav_item.href}
-								className="text-gray-900 group-hover:opacity-50 transition-opacity"
+								href={item.href}
+								className={classnames({
+									'text-gray-900': item.href !== currentPath,
+									'text-blue-500 font-semibold': item.href === currentPath,
+									'group-hover:opacity-50 transition-opacity': true
+								})}
 							>
-								{nav_item.label}
+								{item.label}
 							</Link>
 						</li>
 					)
