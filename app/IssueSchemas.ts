c77791be0ae72ad9663ef82842f475ccb1850d0e
Trieu <phamhai.trieu@hachi-x.com>
2024-05-26 17:11:10 +0700

    :v: [back] Schema: Change schema according to DB

--- app/IssueSchemas.ts
+++ app/IssueSchemas.ts
@@ -4,12 +4,13 @@ export interface IssueInterface {
 	id: number;
 	title: string;
 	description: string;
-	status: EnumValues
-	createdAt: Date,
+	status: string;
+	createdAt: Date;
 	updatedAt: Date
 }
 
-export const createIssueSchema = z.object({
+export const IssueSchema = z.object({
 	title: z.string().min(1, "Title is required!").max(255, "Title is too long! Must be 255 characters or less!"),
-	description: z.string().min(1, "Description is required!")
+	description: z.string().min(1, "Description is required!"),
+	status: z.string()
 });
